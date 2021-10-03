eabd0413d0d4ce521841cb33d59c2268b8f0743d
David Karlsson <sjappskalle@gmail.com>
2021-10-03 14:08:45 +0200

    Add load more functionality

--- frontend/App/App.tsx
+++ frontend/App/App.tsx
@@ -5,10 +5,14 @@ import Loader from "react-loader-spinner";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
   const [users, setUsers] = useState([]);
+  const [nextPage, setNextPage] = useState(2);
+
+  const baseUrl = "http://localhost:4000/api";
 
   const fetchData = () => {
-    return fetch("http://localhost:4000/api/")
+    return fetch(baseUrl + "?results=8")
       .then((response) => response.json())
       .then((data) => setUsers(data));}
 
@@ -16,6 +20,38 @@ function App() {
     fetchData()
       .then(() => setLoading(false))
   }, []);
+
+  const showMore = () => {
+    setLoadingMore(true)
+    return fetch(baseUrl + "?results=8&page=" + nextPage)
+      .then((response) => response.json())
+      .then((data) => {
+
+        const newUsers = users.concat(data);
+        setUsers(newUsers);
+
+        const page = nextPage + 1
+        setNextPage(page)
+        setLoadingMore(false)
+      });
+  };
+
+  const loadButton = () => {
+    if(loadingMore) {
+      return (
+       <Loader
+          type="BallTriangle"
+          color="#575453"
+          height={20}
+          width={20}
+        />
+      )
+    } else {
+      return (
+        <button onClick={showMore} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Visa fler</button>
+      )
+    }
+  }
 
   return (
     <div className="min-h-screen p-5 pb-10 bg-gray-100">
@@ -32,8 +68,14 @@ function App() {
           </div>
         }
         { !loading && 
-          <List users={users}/>
+          <div>
+            <List users={users}/>
+            <div className="flex justify-center mt-5">
+              { loadButton() }
+            </div>
+          </div>
         }
+      
       </div>
     </div>
   );
--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const request = require('request');
+const qs = require('qs');
 
 const app = express();
 app.use(cors({origin: 'http://localhost:8080'}));
 
 const port = process.env.PORT || 4000;
 
-const parseBody = (users: any) => {
-  const parsed_users = users.map((user: any) => {
+const buildObject = (users: any) => {
+  return users.map((user: any) => {
     return {
       name: user.name.first,
       country :user.location.country,
       picture: user.picture.medium,
     }
   })
+}
 
-  return parsed_users;
+const searchParams = (query: any) => { 
+  return qs.stringify(query, { encode: false })
 }
 
 app.get('/api', (req: any, res: any) => {
-  request.get("https://randomuser.me/api/?results=50", (err: any, response: any, body: any) => {
-    const response_users = parseBody(JSON.parse(body).results);
+  request.get("https://randomuser.me/api?" + searchParams(req.query), (err: any, response: any, body: any) => {
+    const response_users = buildObject(JSON.parse(body).results);
     res.send(response_users);
   });
 });
